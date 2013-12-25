6a448e035fdeec17d43d76117bbf6df6b5dae274
Tom <chonski1@gmail.com>
2013-12-25 11:47:48 +1100

    Original code

---
+++ isMultiple/src/isMultiple.java
@@ -0,0 +1,14 @@
+public class isMultiple {
+	public static void main(String[] args) {
+		long n = 40000000000L;
+		long m = 20000000;
+		// int i;
+		
+		if (n % m == 0) {
+			System.out.println("n is a multiple of m");
+		} else {
+			System.out.println("n is not a multiple of m");
+		}
+	}
+	
+}
